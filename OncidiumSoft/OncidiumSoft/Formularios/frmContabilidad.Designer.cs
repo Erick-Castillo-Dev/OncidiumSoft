e4ae92ff3d5669c0fff365391ca96c5a77b934d0
MyFirstApp <pepe_chiquilin_@hotmail.com>
2016-10-29 21:42:49 -0500

    Realización de formularios frmContabilidad, frmGastosEspecificos y frmHistorialContabilidades

---
+++ OncidiumSoft/OncidiumSoft/Formularios/frmContabilidad.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace OncidiumSoft.Formularios
+{
+    partial class frmContabilidad
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GastosGenerales = new System.Windows.Forms.Button();
+            this.lblFechaA = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblVentasT = new System.Windows.Forms.Label();
+            this.txtGastosGenerales = new System.Windows.Forms.TextBox();
+            this.btnHistorial = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.txtSalarios = new System.Windows.Forms.TextBox();
+            this.lblSalarios = new System.Windows.Forms.Label();
+            this.lblFecha = new System.Windows.Forms.Label();
+            this.lblGanancia = new System.Windows.Forms.Label();
+            this.lblInversion = new System.Windows.Forms.Label();
+            this.lblGastosGenerales = new System.Windows.Forms.Label();
+            this.lblVentas = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // GastosGenerales
+            // 
+            this.GastosGenerales.AccessibleName = "btnGastosGenerales";
+            this.GastosGenerales.BackColor = System.Drawing.Color.Transparent;
+            this.GastosGenerales.Location = new System.Drawing.Point(233, 61);
+            this.GastosGenerales.Name = "GastosGenerales";
+            this.GastosGenerales.Size = new System.Drawing.Size(20, 20);
+            this.GastosGenerales.TabIndex = 32;
+            this.GastosGenerales.UseVisualStyleBackColor = false;
+            // 
+            // lblFechaA
+            // 
+            this.lblFechaA.AutoSize = true;
+            this.lblFechaA.Location = new System.Drawing.Point(306, 29);
+            this.lblFechaA.Name = "lblFechaA";
+            this.lblFechaA.Size = new System.Drawing.Size(29, 13);
+            this.lblFechaA.TabIndex = 31;
+            this.lblFechaA.Text = "label";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(139, 169);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 13);
+            this.label2.TabIndex = 30;
+            this.label2.Text = "0.00";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(115, 121);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 29;
+            this.label1.Text = "0.00";
+            // 
+            // lblVentasT
+            // 
+            this.lblVentasT.AutoSize = true;
+            this.lblVentasT.Location = new System.Drawing.Point(91, 29);
+            this.lblVentasT.Name = "lblVentasT";
+            this.lblVentasT.Size = new System.Drawing.Size(28, 13);
+            this.lblVentasT.TabIndex = 28;
+            this.lblVentasT.Text = "0.00";
+            // 
+            // txtGastosGenerales
+            // 
+            this.txtGastosGenerales.Location = new System.Drawing.Point(133, 61);
+            this.txtGastosGenerales.Name = "txtGastosGenerales";
+            this.txtGastosGenerales.Size = new System.Drawing.Size(100, 20);
+            this.txtGastosGenerales.TabIndex = 27;
+            // 
+            // btnHistorial
+            // 
+            this.btnHistorial.Location = new System.Drawing.Point(318, 183);
+            this.btnHistorial.Name = "btnHistorial";
+            this.btnHistorial.Size = new System.Drawing.Size(113, 28);
+            this.btnHistorial.TabIndex = 26;
+            this.btnHistorial.Text = "Ver historial";
+            this.btnHistorial.UseVisualStyleBackColor = true;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(356, 139);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.TabIndex = 25;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(257, 139);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 24;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            // 
+            // txtSalarios
+            // 
+            this.txtSalarios.Location = new System.Drawing.Point(330, 61);
+            this.txtSalarios.Name = "txtSalarios";
+            this.txtSalarios.Size = new System.Drawing.Size(100, 20);
+            this.txtSalarios.TabIndex = 23;
+            // 
+            // lblSalarios
+            // 
+            this.lblSalarios.AutoSize = true;
+            this.lblSalarios.Location = new System.Drawing.Point(265, 66);
+            this.lblSalarios.Name = "lblSalarios";
+            this.lblSalarios.Size = new System.Drawing.Size(59, 13);
+            this.lblSalarios.TabIndex = 22;
+            this.lblSalarios.Text = "Salarios:  $";
+            // 
+            // lblFecha
+            // 
+            this.lblFecha.AutoSize = true;
+            this.lblFecha.Location = new System.Drawing.Point(265, 29);
+            this.lblFecha.Name = "lblFecha";
+            this.lblFecha.Size = new System.Drawing.Size(40, 13);
+            this.lblFecha.TabIndex = 21;
+            this.lblFecha.Text = "Fecha:";
+            // 
+            // lblGanancia
+            // 
+            this.lblGanancia.AutoSize = true;
+            this.lblGanancia.Location = new System.Drawing.Point(25, 169);
+            this.lblGanancia.Name = "lblGanancia";
+            this.lblGanancia.Size = new System.Drawing.Size(118, 13);
+            this.lblGanancia.TabIndex = 20;
+            this.lblGanancia.Text = "Ganancia o pérdida:   $";
+            // 
+            // lblInversion
+            // 
+            this.lblInversion.AutoSize = true;
+            this.lblInversion.Location = new System.Drawing.Point(25, 121);
+            this.lblInversion.Name = "lblInversion";
+            this.lblInversion.Size = new System.Drawing.Size(94, 13);
+            this.lblInversion.TabIndex = 19;
+            this.lblInversion.Text = "Inversión total:    $";
+            // 
+            // lblGastosGenerales
+            // 
+            this.lblGastosGenerales.AutoSize = true;
+            this.lblGastosGenerales.Location = new System.Drawing.Point(25, 66);
+            this.lblGastosGenerales.Name = "lblGastosGenerales";
+            this.lblGastosGenerales.Size = new System.Drawing.Size(108, 13);
+            this.lblGastosGenerales.TabIndex = 18;
+            this.lblGastosGenerales.Text = "Gastos en general:  $";
+            // 
+            // lblVentas
+            // 
+            this.lblVentas.AutoSize = true;
+            this.lblVentas.Location = new System.Drawing.Point(25, 29);
+            this.lblVentas.Name = "lblVentas";
+            this.lblVentas.Size = new System.Drawing.Size(70, 13);
+            this.lblVentas.TabIndex = 17;
+            this.lblVentas.Text = "Ventas:       $";
+            // 
+            // frmContabilidad
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(456, 240);
+            this.Controls.Add(this.GastosGenerales);
+            this.Controls.Add(this.lblFechaA);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblVentasT);
+            this.Controls.Add(this.txtGastosGenerales);
+            this.Controls.Add(this.btnHistorial);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtSalarios);
+            this.Controls.Add(this.lblSalarios);
+            this.Controls.Add(this.lblFecha);
+            this.Controls.Add(this.lblGanancia);
+            this.Controls.Add(this.lblInversion);
+            this.Controls.Add(this.lblGastosGenerales);
+            this.Controls.Add(this.lblVentas);
+            this.Name = "frmContabilidad";
+            this.Text = "frmContabilidad";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button GastosGenerales;
+        private System.Windows.Forms.Label lblFechaA;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblVentasT;
+        private System.Windows.Forms.TextBox txtGastosGenerales;
+        private System.Windows.Forms.Button btnHistorial;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.TextBox txtSalarios;
+        private System.Windows.Forms.Label lblSalarios;
+        private System.Windows.Forms.Label lblFecha;
+        private System.Windows.Forms.Label lblGanancia;
+        private System.Windows.Forms.Label lblInversion;
+        private System.Windows.Forms.Label lblGastosGenerales;
+        private System.Windows.Forms.Label lblVentas;
+    }
+}
