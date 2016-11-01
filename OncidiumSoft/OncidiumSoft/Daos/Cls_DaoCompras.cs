9245b531a78ea6983f871749688963447985abd1
EdgarCoronaRD <edgarcoron101@gmail.com>
2016-10-31 18:23:27 -0600

    Agregacion de ventana pedios y compras

---
+++ OncidiumSoft/OncidiumSoft/Daos/Cls_DaoCompras.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OncidiumSoft.Daos
+{
+    class Cls_DaoCompras
+    {
+        
+
+        Conexion ca = new Conexion();
+    }
+}
